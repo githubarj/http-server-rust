7d940300de8fe71263908463ecea91f8189f857f
Richard Githuba <githuba9520@gmail.com>
2024-06-18 12:05:17 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
-
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
-    
+    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
-
-    
     for stream in listener.incoming() {
         match stream {
-            Ok( mut stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let request_line = http_request[0].split(" ").collect::<Vec<&str>>();
+    let resp = match request_line[1] {
+        "/" => "200 OK",
+        _ => "404 Not Found",
+    };
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = format!("HTTP/1.1 {}\r\n\r\n", resp);
+    stream.write_all(response.as_bytes()).unwrap();
 }
